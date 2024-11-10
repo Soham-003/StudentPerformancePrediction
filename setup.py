cc126fd40a4ba96984f046fc38729e09520a53d1
sohamsawankhanna@gmail.com <sohamsawankhanna@gmail.com>
2024-11-10 14:16:38 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ def get_requirements(file_path : str)-> List[str]:
     # function will returning list of requirements
     requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements=file_obj.readlines() #reads the entire file into a list, but it includes newline characters (\n) at the end of each line.
+        requirements=[req.replace("\n","") for req in requirements] #removing those extra newline characters, ensuring that each requirement is a clean string.
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
 setup(
-
+    #metadata
     name = 'MLproject',
     version = '0.0.1',
     author = "Soham",
